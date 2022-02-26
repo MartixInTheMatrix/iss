f4d5704263e118b89b564f03631c8784548a8f8b
Martix <66840932+MartixInTheMatrix@users.noreply.github.com>
2022-02-26 15:36:27 +0100

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Schema } from './schema';
 let window = new Schema(100, 26, 0.1);
 
-window.getLocation()
-setTimeout(()=>{
+window.getLocation().then(()=>{
     console.log(window.x)
     console.log(window.y)
+    window.theme('.')
     console.log(window.build());
-}, 2000)
+})
+
+
--- src/schema.ts
+++ src/schema.ts
@@ -3,23 +3,31 @@ export class Schema {
     width: number;
     height: number;
     reduction: number;
-    x: any;
-    y: any;
-    
+    x: number | undefined;
+    y: number | undefined;
+    private style: string;
+
     constructor(width: number, height: number, reduction: number){
         this.width = width;
         this.height = height;
         this.reduction = reduction;
         this.x = undefined;
         this.y = undefined;
+        this.style = '.';
     }
 
-    getLocation = async ()=>{
+    async getLocation(){
+        return new Promise(resolve => {
+            
         axios.get('http://api.open-notify.org/iss-now.json').then(res =>{
             console.log(res.data)
-            this.x = (res.data.iss_position.latitude * this.reduction).toFixed(0)
-            this.y = (res.data.iss_position.longitude * this.reduction).toFixed(0)
+
+            this.x = Number((res.data.iss_position.latitude * this.reduction).toFixed(0))
+            this.y = Number((res.data.iss_position.longitude * this.reduction).toFixed(0))
+            
+            resolve(res.data)
         })
+    });
     }
 
     build(){
@@ -30,10 +38,15 @@ export class Schema {
             let str = ''
 
             for(let i= this.width / 2 - this.width; i < this.width / 2; i++){
-                bool && this.x == i? str += '@' : earth && i == 0? str += 'E' : str += '.'
+                bool && this.x == i? str += '@' : earth && i == 0? str += 'E' : str += this.style
             }
             builder.push(str)
         }
         return builder.join("\n")
+    }
+
+    theme(theme:string){
+        if(theme.length != 1){return console.log('Invalid theme !')}
+        this.style = theme
     }
 }
